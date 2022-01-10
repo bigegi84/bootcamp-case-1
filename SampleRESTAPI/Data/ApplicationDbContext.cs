dcb968e88696f924692c1ba7e1ed498926f008ce
Gilang Pratama Wiguna <gilangpw84@gmail.com>
2022-01-10 09:22:05 +0700

    init

---
+++ SampleRESTAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SampleRESTAPI.Models;
+
+namespace SampleRESTAPI.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions options):base(options)
+        {
+        }
+
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+
+        /*protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Student>().ToTable("Mahasiswa");
+        }*/
+    }
+}
